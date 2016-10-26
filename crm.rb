a6c771ea0d4598d109e3a30b1594b0ce173c919d
Yuan He <yuanthehe@gmail.com>
2016-10-26 15:02:26 -0400

    everything should be working but its not

--- crm.rb
+++ crm.rb
@@ -2,9 +2,9 @@ require_relative 'contact'
 require 'sinatra'
 
 #fake data
-Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
-Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
-Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+#Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+#Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+#Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
 get '/' do
   @crm_app_name = "Yuan's CRM"
@@ -17,4 +17,9 @@ end
 
 get '/contacts/new' do
   erb :new_contact
+end
+
+post '/contacts' do
+  Contact.create(params[:first_name], params[:last_name], params[:email], params[:note])
+  redirect to('/contacts')
 end
