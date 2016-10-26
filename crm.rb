d1c34606e443e21bfda9a227862683444130c5aa
Yuan He <yuanthehe@gmail.com>
2016-10-26 14:31:04 -0400

    created fake data and contacts.erb

--- crm.rb
+++ crm.rb
@@ -1,4 +1,10 @@
+require_relative 'contact'
 require 'sinatra'
+
+#fake data
+Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
 
 get '/' do
   @crm_app_name = "Yuan's CRM"
